f103600a0aab95ac4684cf330f03229cbc533a9b
Anton <ponochovnyi.anton@LLL.kpi.ua>
2024-05-25 00:53:29 +0300

    Add Ansi patterns

--- regexPatterns.js
+++ regexPatterns.js
@@ -1,14 +1,32 @@
-const formattingPatterns = [
+const formattingHTML = {
+    type: "HTML",
+    regExp: [
+        {pattern: /\`\`\`([\s\S]+?)\`\`\`/g, replacement: "<pre>$1</pre>"},
+        {pattern: /^\s*$/gm, replacement: "<p></p>"},
+        {pattern: /\*\*([^ ].*[^ ])\*\*/g, replacement: "<b>$1</b>",},
+        {pattern: /\*\*([^ ])\*\*/g, replacement: "<b>$1</b>",},
+        {pattern: /\_([^ ].*[^ ])\_/g, replacement: " <em>$1</em> "},
+        {pattern: /\_([^ ])\_/g, replacement: " <em>$1</em> "},
+        {pattern: /\`([^ ].*[^ ])\`/g, replacement: " <tt>$1</tt> "},
+        {pattern: /\`([^ ])\`/g, replacement: " <tt>$1</tt> "},
+    ],
+};
 
-    { pattern: /\`\`\`([\s\S]+?)\`\`\`/g, replacement: "<pre>$1</pre>" },
-    { pattern: /^\s*$/gm, replacement: "<p></p>" },
-    { pattern: /\*\*([^ ].*[^ ])\*\*/g, replacement: "<b>$1</b>", },
-    { pattern: /\*\*([^ ])\*\*/g, replacement: "<b>$1</b>", },
-    { pattern: /\_([^ ].*[^ ])\_/g, replacement: " <em>$1</em> " },
-    { pattern: /\_([^ ])\_/g, replacement: " <em>$1</em> " },
-    { pattern: /\`([^ ].*[^ ])\`/g, replacement: " <tt>$1</tt> " },
-    { pattern: /\`([^ ])\`/g, replacement: " <tt>$1</tt> " },
+const formattingAnsi = {
+    type: "ANSI",
+    regExp: [
+        {
+            pattern: /\`\`\`([\s\S]+?)\`\`\`/g,
+            replacement:
+                "\x1b[7mPREFORMATED_PLACEHOLDER$1PREFORMATED_PLACEHOLDER\x1b[0m",
+        },
+        { pattern: /\*\*([^ ].*[^ ])\*\*/g, replacement: "\x1b[1m$1\x1b[0m", },
+        { pattern: /\*\*([^ ])\*\*/g, replacement: "\x1b[1m$1\x1b[0m", },
+        { pattern: /\_([^ ].*[^ ])\_/g, replacement: "\x1b[3m$1\x1b[0m" },
+        { pattern: /\_([^ ])\_/g, replacement: "\x1b[3m$1\x1b[0m" },
+        { pattern: /\`([^ ].*[^ ])\`/g, replacement: "\x1b[7m$1\x1b[0m" },
+        { pattern: /\`([^ ])\`/g, replacement: "\x1b[7m$1\x1b[0m" },
+    ],
+};
 
-];
-
-module.exports = { formattingPatterns }
+module.exports = { formattingHTML, formattingAnsi }
